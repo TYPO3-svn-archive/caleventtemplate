518e48840df8574240ac984131d21238df03e5d2
thomaskowtsch <thomaskowtsch@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-02-23 09:48:18 +0000

    follow coding guidelines
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caleventtemplate/trunk@83950 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -35,8 +35,8 @@ $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='la
 
 
 t3lib_extMgm::addPlugin(array(
-	'LLL:EXT:caleventtemplate/locallang_db.xml:tt_content.list_type_pi1',
-	$_EXTKEY . '_pi1',
+	'LLL:EXT:caleventtemplate/locallang_db.xml:tt_content.list_type_pi1', 
+	$_EXTKEY . '_pi1', 
 	t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif'
-),'list_type');
+), 'list_type');
 ?>
